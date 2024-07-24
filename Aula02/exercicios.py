852d33f2df41a7264e70ebd38aadc39465cc5759
Marcelo <marcelo.kniss@outlook.com>
2024-07-24 18:04:24 -0300

    Adicionado resoluções

--- Aula02/exercicios.py
+++ Aula02/exercicios.py
@@ -1,5 +1,10 @@
 #Inteiros (int)
 #Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+"""
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+print(f"A soma dos numeros é: {numero1+numero2}")
+"""
 #Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 #Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 #Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
