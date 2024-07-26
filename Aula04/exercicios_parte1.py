0687a690cf23d25fc1fd2b4bc57fbd63cacb9384
Marcelo <marcelo.kniss@outlook.com>
2024-07-26 18:59:38 -0300

    Adicionado exercicios sobre listas, dicionarios

---
+++ Aula04/exercicios_parte1.py
@@ -0,0 +1,60 @@
+# typing, listas e dicionário
+#O Python é uma linguagem dinamica, forte
+"""
+1- Type Hint 
+ele ajuda o desenvolvedor a se comunicar melhor
+assim a gente sabe oque a variavel
+foi criada para aquele proposito (Hint)
+"""
+"""
+idade: int = 30
+altura: float = 1.75
+nome: str = "Marcelo"
+is_estudante: bool = True
+"""
+
+#LISTAS 
+"""
+
+produto: str = "sapato"
+produto_2: str = "camiseta"
+produto_3: str = "videogame"
+
+produtos: list = []
+produtos.append(produto)
+produtos.append(produto_2)
+produtos.append(produto_3)
+
+produtos.pop() #tira o ultimo produto que entrou
+#produtos.remove(produto_3) #ou escolhe o item a tirar
+
+print(produtos)
+"""
+
+# DICIONARIOS
+nome = "sapato"
+quantidade = 39
+preco = 10.38,
+disponibilidade = True
+"""
+dicionario = {
+    "nome":"sapato",
+    "quantidade":39,
+    "preco":10.38,
+    "disponibilidade":True
+}
+"""
+#Crie um dicionario para armazenar informações de um livro,
+#incluindo titulo, autor e ano de publicação. Imprima cada informação.
+
+from typing import Dict, Any
+livro: dict[str, Any] = {
+    "Titulo":"Game of Thrones",
+    "Autor":"N/A",
+    "Ano":2005
+}
+
+lista_elementos:list = livro.items()
+
+for elemento in lista_elementos:
+    print(elemento)
