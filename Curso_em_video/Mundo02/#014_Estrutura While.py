6344bc18595ec42e1a93ec671da3511d074920e8
Marcelo <marcelo.kniss@outlook.com>
2024-08-06 18:28:37 -0300

    While

---
+++ Curso_em_video/Mundo02/#014_Estrutura While.py
@@ -0,0 +1,36 @@
+"""
+for c in range(1,11):
+    print(c)
+print('fim')
+"""
+"""
+c = 1
+while c < 10:
+    print(c)
+    c = c + 1 # c+= 1
+print('fim')
+"""
+"""
+n = 1
+while n != 0:
+    n = int(input("Digite um numero: "))
+print('FIM')
+"""
+
+"""
+r = 's'
+while r == 's':
+    n = str(input("Digite um valor: "))
+    r = str(input("Quer continuar? S/N: ")).lower()
+print("FIM")
+"""
+n = 1
+impar = par = 0
+while n != 0:
+    n = int(input("Digite um número: "))
+    if n % 2 == 0 :
+        par += 1
+    if n % 2 != 0 :
+        impar += 1
+print(f"Números pares: {par}")
+print(f"Números ímpares {impar}")
