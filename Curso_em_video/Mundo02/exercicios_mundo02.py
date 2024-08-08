c35e757f62136e9fdc659b09de949a321edbc3e4
Marcelo <marcelo.kniss@outlook.com>
2024-08-08 19:52:19 -0300

    Novos Exercicios

--- Curso_em_video/Mundo02/exercicios_mundo02.py
+++ Curso_em_video/Mundo02/exercicios_mundo02.py
@@ -280,3 +280,20 @@ print(f"A Média de idade do grupo é: {int(media_idade)}")
 print(f"O homem mais velho tem {maior_idade_homem} anos e e chama {nome_mais_velho}")
 print(f"Menores de 20 e mulheres: {menor_vinte}")
 """
+
+#57
+#Faça um programa que leia o sexo de uma pessoa
+#mas só aceite os valores "M" ou "F".
+#Caso esteja errado, peça a digitação novamente
+#até ter um valor correto.
+
+sexo = str(input("Digite seu sexo: ")).strip().lower()[0]
+while sexo not in "mMfF":
+    sexo = str(input("Entrada inválida. Digite M ou F: "))
+    
+if sexo == 'm' or sexo =='f':
+    if sexo =='m':
+        print("Entrada inserida com sucesso! Sexo Masculino!")
+    elif sexo == 'f':
+        print("Entrada inserida com sucesso! Sexo Feminino!")
+print("Bye!")
