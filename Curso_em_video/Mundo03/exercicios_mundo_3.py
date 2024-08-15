8de10f7148a951e0daef76a78f5f701f1298a647
Marcelo <marcelo.kniss@outlook.com>
2024-08-14 21:45:04 -0300

    OK

--- Curso_em_video/Mundo03/exercicios_mundo_3.py
+++ Curso_em_video/Mundo03/exercicios_mundo_3.py
@@ -58,3 +58,71 @@ print("=" * 20)
 print(f"Time 'Chapecoense' está posição número {times.index("Chapecoense")}")
 
 """
+
+# 74
+# Crie um programa que vai gerar cinco números aleatórios 
+# e colocar em uma tupla.
+# Depois disso, mostre a listagem de números gerados
+# e também indice o menor e o maior valor que estão na tupla
+
+""" 
+COM FOR 
+
+numeros = []
+
+from random import randint
+
+for i in range (0,5):
+    numeros.append(randint(0,999))
+
+numeros_tupla = tuple(numeros)
+
+print(f"Tupla Gerada: {numeros_tupla}")
+print(f"O maior número da tupla é o {max(numeros_tupla)} e menor número é o {min(numeros_tupla)}")
+
+"""
+"""
+COM WHILE
+from random import randint
+numeros = []
+n = 1
+while n <6:
+    numeros.append(randint(1,99))
+    n += 1
+
+tupla = tuple(numeros)
+
+print(f"Valores da Tupla: {tupla}")
+print(f"Maior valor: {max(tupla)}")
+print(f"Menor valor: {min(tupla)}")
+"""
+
+# 75
+# Desenvolva um programa que leia quatro valores pelo teclado
+# e guarde-os em uma tupla. # No final, mostre:
+
+# a) quantas vezes apareceu o valor 9
+# b) em que posição foi digitado o primeiro valor 3
+# b) quais foram os números pares
+
+n_tupla = []
+for i in range(1,5):
+    n = int(input(f"Digite o número {i}: "))
+    n_tupla.append(n)
+
+n_tupla = tuple(n_tupla)
+
+# Quantas vezes aparece o número 9?
+print(f"O Número 9 apareceu {n_tupla.count(9)} vezes")
+
+# Em que posição foi digitado o primeiro valor 3
+if 3 in n_tupla:
+    print(f"O Primeiro valor 3 foi digitado no espaço de memória nº {n_tupla.index(3)}")
+else:
+    print("Não foi digitado nenhum número 3")
+# Quais foram os números pares ?
+pares = []
+for num in n_tupla:
+    if num % 2 == 0:
+        pares.append(num)
+print(f"Números Pares: {pares}")
